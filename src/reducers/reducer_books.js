d2e44e6a65300ac08dfec0dd58e2794c2e7ab6d1
gjkthx <gjkthx@gmail.com>
2017-10-31 12:49:39 -0600

    learning redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function(){
+    return[
+        {title: 'It', pages: 500},
+        {title: 'Of Mice and Men', pages: 20},
+        {title: 'Mobydick', pages: 1000},
+        {title: 'I Kill Giants', pages: 100},
+        {title: 'Fun fun fun', pages: 53621}
+    ]
+}
